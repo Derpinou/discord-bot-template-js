1a391e0a849f42d2d65eb3de542b7cee870fac91
Derp <60037058+Derpinou@users.noreply.github.com>
2020-09-24 08:56:45 +0200

    Update Client.js
    Mise a jour des commentaires

--- Base/Client.js
+++ Base/Client.js
@@ -1,12 +1,14 @@
+//Importation de Client et Collection de discord.js
 const { Client, Collection} = require('discord.js');
+//Extend du Client
 class Bot extends Client {
     constructor(options) {
         super(options);
-        this.prefix = require("../config").prefix
-        this.config = require("../config.js"); // Load the config file
-        this.commands = new Collection(); // Creates new commands collection
-        this.functions = require('../utils/functions.js'); // Load the functions file
-    }
+        this.prefix = require("../config").prefix //Charger le prefix dans le constructeur pour pouvoir utiliser client.prefix
+        this.config = require("../config.js"); // Chargement du fichier de configuration
+        this.commands = new Collection(); // Déclaration de la nouvelle Collection commands
+        this.functions = require('../utils/functions.js'); // Chargement du fichier de fonctions
 }
+//Exportation de la class Bot
 module.exports = Bot
 
